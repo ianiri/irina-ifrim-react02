ee1f93c6b2aaa416b82c8b340a46f91f783c19dc
ianiri <44301798+ianiri@users.noreply.github.com>
2022-12-18 11:57:39 +0200

    14dec-reduce-object.values-object.entries

--- react-components/ex02/app.js
+++ react-components/ex02/app.js
@@ -31,3 +31,60 @@ const person = {
     },
   ],
 };
+
+//ex01
+console.warn(`Folosind obiectul person si reduce, afiseaza in consola un string care contine skill-urile de pe pozitiile pare ale arrayului, separate prin virgula`);
+
+const skillArray1 = person.skills.reduce((skillArray1, skill, index) => {
+  if (index % 2 === 0) {
+    skillArray1.push(skill);
+  };
+
+  return skillArray1;
+}, []);
+console.log(skillArray1.toString());
+
+
+//ex02
+console.warn(`In mod similar, afiseaza skill-urile care NU incep cu j.`);
+
+const skillArray2 = person.skills.reduce((skillArray2, skill) => {
+  if (!skill.toLowerCase().startsWith('j')) {
+    skillArray2.push(skill);
+  };
+
+  return skillArray2;
+}, []);
+console.log(skillArray2.join(','));
+
+
+//ex03
+console.warn(`Folosind reduce afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy, xxx yyy."`);
+ 
+const message1 = person.friends.reduce((message1, { name, surname }, index, friends) => {
+  let punctuation = ', ';
+
+  if (index === friends.length - 1) {
+    punctuation = '.';
+  }
+  message1 += `${name} ${surname}${punctuation}`;
+
+  return message1;
+}, 'Prietenii mei se numesc ');
+console.log(message1);
+
+
+//ex06
+console.warn(`Afiseaza fraza: "Intre Dragos si Larry este o diferenta de xx ani. Intre Dragos si Steven... ", doar daca varsta prietenului este impara."`);
+
+const message6 = person.friends.reduce((message6, friend) => {
+  const {age, name} = friend;
+  const diff = person.age - age;
+
+  if (age % 2 !== 0) {
+    message6 += `Intre ${person.name} si ${person.name} este o diferenta de ${diff} ani. `;
+  }
+
+  return message6;
+}, '');
+console.log(message6.trim());
--- react-components/ex03/app.js
+++ react-components/ex03/app.js
@@ -23,3 +23,45 @@ const person = {
 };
 
 
+//ex01
+console.warn(`Folosind Object.values(), afiseaza o lista inversata cu numele complet inversat al prietenilor. `);
+
+const friendObjects = Object.values(person.friends);
+friendObjects.reverse().forEach(({name, surname}) => {
+  console.log(`${surname} ${name}`);
+});
+
+
+//ex02
+console.warn(`Afiseaza propozitia: “Prietenii mei sunt Larry, Steven si Carol.” folosind Object.values() `);
+const message2 = Object.values(person.friends).reduce((message2, friend, index, friends) => {
+  const { name } = friend;
+  let punctuation = ', ';
+
+  if (index === friends.length - 1) {
+    punctuation = '.';
+  }
+
+  message2 += `${name}${punctuation}`;
+
+  return message2;
+
+}, 'Prietenii mei sunt ');
+console.log(message2);
+
+//ex02
+console.warn(`Prin aceeasi metoda, afiseaza propozitia: “Diferenta de varsta intre Larry si Dragos este de xxx ani.” etc…`);
+const message3 = Object.values(person.friends).reduce((message3, { name, age }, index, friends) => {
+  const diff = person.age - age;
+  let punctuation = '. ';
+
+  if (index === friends.length - 1) {
+    punctuation = '.';
+  }
+
+  message3 += `Diferenta de varsta intre ${name} si ${person.name} este de ${diff} ani${punctuation}`;
+
+  return message3;
+
+}, '');
+console.log(message3);
